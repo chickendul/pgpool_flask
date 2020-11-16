23bead72af19f338b19c8659c19170416073d0bb
류재성 <rjs4902@somansa.com>
2020-11-16 22:51:47 +0900

    Initial commit - 2020.11.16  - 최초 skel 생성 후 commit.

---
+++ app/main/index.py
@@ -0,0 +1,27 @@
+from flask import Blueprint, request, render_template, flash, redirect, url_for
+import sqlite3
+import paramiko
+
+def conn_sqlite():
+    conn = sqlite3.connect('./test.db')
+    cur = conn.cursor()
+    cur.execute("select sqlite_version();")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    conn.close()
+    #return cur
+
+main = Blueprint('main', __name__, url_prefix='/')
+
+@main.route('/', methods=['GET'])
+@main.route('/main', methods=['GET'])
+def index():
+    testData = 'testdata'
+
+    return render_template('/index.html', testdatahtml = testData)
+
+def execCommands():
+    cli = paramiko.SSHClient()
+    cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    cli.connect
