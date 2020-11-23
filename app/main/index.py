c3d584f262ee46c9a52c30941aa50e4f4e8b65a7
류재성 <rjs4902@naver.com>
2020-11-23 19:01:00 +0900

    2020.11.23 - bootstrap 페이지 추가

--- app/main/index.py
+++ app/main/index.py
@@ -1,3 +1,35 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:479bfa98e9dffda25a8267e48c927f87a1903b660a7ecb07931beec3264712ef
-size 726
+from flask import Blueprint, request, render_template, flash, redirect, url_for
+import sqlite3
+import paramiko
+
+
+def conn_sqlite():
+    conn = sqlite3.connect('./test.db')
+    cur = conn.cursor()
+    cur.execute("select sqlite_version();")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    conn.close()
+    # return cur
+
+
+main = Blueprint('main', __name__, url_prefix='/')
+
+
+@main.route('/', methods=['GET'])
+@main.route('/main', methods=['GET'])
+def index():
+    testData = 'testdata'
+    return render_template('/index.html', testdatahtml=testData)
+
+
+@main.route('/pgpool', methods=['GET'])
+def pgpool():
+    return render_template('/pgpool.html')
+
+
+def execcommands():
+    cli = paramiko.SSHClient()
+    cli.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+    # cli.connect()
--- app/run.py
+++ app/run.py
@@ -1,3 +1,3 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:19a6401dee9a43e5697c3c16319c6459e13d62853d54694a1063199176d4e7ac
-size 67
+from app import app
+
+app.run(host="127.0.0.1", port=80, debug=True)
